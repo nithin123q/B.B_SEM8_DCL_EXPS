c82ddea68dacb0422f6273bd3293250e1f40b2ff
Nithin Menezes <68021839+nithin123q@users.noreply.github.com>
2022-06-15 17:18:06 +0530

    Add files via upload

---
+++ dcl_Exps/dc_exp3/Exp_3_DC/CalculatorClient.java
@@ -0,0 +1,25 @@
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.net.MalformedURLException; import java.rmi.NotBoundException;
+
+public class CalculatorClient {
+
+public static void main(String[] args) { try {
+Calculator c = (Calculator)
+Naming.lookup( "rmi://localhost/CalculatorService");
+System.out.println( c.sub(10, 3) ); System.out.println( c.add(6, 9) ); System.out.println( c.mul(4, 5) ); System.out.println( c.div(144, 12) );
+}
+catch (MalformedURLException murle) { System.out.println(); System.out.println( "MalformedURLException"); System.out.println(murle);
+}
+catch (RemoteException re) { System.out.println(); System.out.println(
+"RemoteException"); System.out.println(re);
+}
+catch (NotBoundException nbe) { System.out.println(); System.out.println(
+"NotBoundException"); System.out.println(nbe);
+}
+catch (
+java.lang.ArithmeticException
+ae) { System.out.println(); System.out.println( "java.lang.ArithmeticException"); System.out.println(ae);
+}
+}
+}
